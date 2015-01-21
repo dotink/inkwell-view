09d18828ef2a3b5584745aa609336d1e2ccce991
Matthew J. Sahagian <msahagian@dotink.org>
2015-01-20 21:57:59 -0800

    Composer cleanup and comment update

--- src/View.php
+++ src/View.php
@@ -5,7 +5,8 @@
 	use Dotink\Flourish;
 
 	/**
-	* View Class
+	* Views provide a simple object interface for data and partial aggregation as well as
+	* interfaces for making templating easier.
 	*
 	* @copyright Copyright (c) 2015, Matthew J. Sahagian
 	* @author Matthew J. Sahagian [mjs] <msahagian@dotink.org>
